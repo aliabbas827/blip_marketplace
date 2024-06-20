22efadcc4099f12b901051dbd05fffd3db60d938
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-06-21 02:54:20 +0500

    Update delivery component positioning and text styling

--- src/components/Delivery/Delivery.jsx
+++ src/components/Delivery/Delivery.jsx
@@ -10,10 +10,9 @@ const Delivery = () => {
                     <h1 className='text-[30px] lg:text-[36px] xl:text-[40px] font-bold'>Delivery Zones</h1>
                     <p className='w-[60%] sm:w-full text-center text-[16px] lg:text-[18px] xl:text-[22px] '>Currently serving select areas. More coming soon!</p>
                 </div>
-                <div className='bg-map md:h-[70vh]  lg:h-[100vh] xl:h-[140vh] mt-20 rounded-t-[60px] relative hidden md:block'>
+                <div className='bg-map md:h-[70vh]  lg:h-[90vh] xl:h-[90vh] mt-20 rounded-t-[60px] relative hidden md:block'>
                     <div className='absolute bottom-[-25vh]'>
                         <img src={images.drone} alt="" />
-
                     </div>
                     <div className='absolute md:bottom-[-49px] lg:bottom-[-20px] xl:bottom-2 md:right-[36%] lg:right-[37%] '>
                         <div className='flex flex-col items-center justify-center md:gap-3 lg:gap-6 xl:gap-8'>
@@ -22,11 +21,10 @@ const Delivery = () => {
                                 <p className='md:text-[12px] lg:text-[15px] xl:text-[22px] text-white'>For QSRs, Restaurants, and Retailers</p>
                             </div>
                             <button className='bg-white text-black md:text-[10px] lg:text-[13px] xl:text-[15px] font-bold px-5 lg:px-7 py-2 lg:py-3 rounded-[10px] tracking-wider'>BECOME A PARTNER</button>
-
                         </div>
                     </div>
                 </div>
-                <div className='bg-bottom  rounded-b-[60px]  md:h-[80vh]  lg:h-[100vh] xl:h-[120vh] md:flex flex-col items-center justify-center gap-4 md:gap-8 hidden '>
+                <div className='bg-bottom  rounded-b-[60px]  md:h-[80vh]  lg:h-[90vh] xl:h-[90vh] md:flex flex-col items-center justify-center gap-4 md:gap-8 hidden '>
                     <h1 className='text-[30px] lg:text-[36px] xl:text-[40px] font-bold tracking-wide '>Download the App Now</h1>
                     <img src={images.qrcode} alt="" className='md:w-28 lg:w-32 xl:w-36' />
                     <div className='flex items-center gap-5'>
